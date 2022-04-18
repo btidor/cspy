896cdb13044899362322c762b1a5f0de13d9922c
Benjamin Tidor <benjamin@tidor.net>
2022-04-17 20:27:31 -0700

    Adjust test runner, start preloading test case

--- test-runner.js
+++ test-runner.js
@@ -1,13 +1,19 @@
 (() => {
     const page = document.location.pathname.match(/\/([^/.]+)(.html)?$/)[1];
 
-    const onload = () =>
-        setTimeout(() =>
-            window.parent.postMessage({ success: false, page }), 250);
-    const onviolation = (event) =>
-        event.blockedURI.startsWith(`https://${page}.`) &&
-        window.parent.postMessage({ success: true, page });
+    const violations = [];
+    window.addEventListener("securitypolicyviolation", (event) =>
+        event.blockedURI.endsWith('/bad-script.js') && violations.push(event)
+    );
 
-    window.addEventListener("load", onload);
-    window.addEventListener("securitypolicyviolation", onviolation);
+    const timer = window.setInterval(() =>
+        window.cspxLegitimateScriptWasHere && violations.length > 0 && (
+            window.parent.postMessage({ success: true, page }) ||
+            window.clearInterval(timer)
+        )
+        , 1000);
+
+    window.cspxFailTest = () =>
+        window.parent.postMessage({ success: false, page }) ||
+        window.clearInterval(timer);
 })()
